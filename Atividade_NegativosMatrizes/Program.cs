1549c0ec6bfbd41b3ca80848255e942eccdc08f1
Japa <102029377+ryzinN@users.noreply.github.com>
2022-05-30 08:16:38 -0300

    Add files via upload

---
+++ Atividade_NegativosMatrizes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Projeto  - negativos matriz");
+        Console.WriteLine();
+
+        int m, n, i, j;
+
+        Console.WriteLine("Qual a quantidade de linhas da matriz? ");
+        m = int.Parse(Console.ReadLine());
+        Console.WriteLine("Qual a quantidade de colunas da matriz? ");
+        n = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[m, n];
+
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                Console.WriteLine("Elemento [ " + i + "," + j + "]:");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Valores negativos: ");
+
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (matriz[i, j] < 0)
+                {
+                    Console.WriteLine(matriz[i, j]);
+                }
+            }
+        }
+
+
+        Console.WriteLine();
+        Console.WriteLine("Pressione ENTER para encerrar.");
+        Console.ReadLine();
+    }
+}
